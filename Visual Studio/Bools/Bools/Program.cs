03f7b3630882e2e6e0cc12a8b152d3d4798e4644
Giancarlo <77162507+GiancarloAcevedo@users.noreply.github.com>
2023-03-20 23:22:22 -0400

    Add files via upload

--- Visual Studio/Bools/Bools/Program.cs
+++ Visual Studio/Bools/Bools/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace UsandoBooleanos
+{
+    class program
+    {
+        //Leccion 5: Usando Variables Booleanas
+
+        static void Main(String[] args)
+        {
+            //signos que se pueden usar: > >= < <= == !=
+
+            bool miBool = true;
+            miBool = 6 != 5;
+            Console.WriteLine(miBool);
+
+            int losNiños = 17;
+            bool sonAdultos = losNiños > 18;
+            Console.WriteLine("Los niños son adultos? {0}", sonAdultos);
+        }
+    }
+}
